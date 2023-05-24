48b4d74cdf1772c0f2d254a637fbb002f393cf60
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-24 13:07:11 -0700

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,8 +10,8 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str):
     """Define a method  that returns the log message obfuscated"""
     for field in fields:
-        message = re.sub(r"{}=(.*?){}".format(field, separator),
-                         f"{field}={redaction}{separator}", message)
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
     return message
 
 
