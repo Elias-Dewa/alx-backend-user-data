17351fa3d1d46a7af927234ff618117375613fa4
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-30 08:57:45 -0700

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -21,8 +21,9 @@ class Auth():
         return True
 
     def authorization_header(self, request=None) -> str:
-        """returns None - request will be the Flask request object"""
-        return None
+        """return the value of the header request Authorization"""
+        if request:
+            return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """returns None - request will be the Flask request object"""
