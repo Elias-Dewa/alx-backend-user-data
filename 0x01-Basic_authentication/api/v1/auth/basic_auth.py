dd28961c8d9215550b1078f46ed01d88b442263f
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-30 13:34:38 -0700

    task 9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -35,3 +35,16 @@ class BasicAuth(Auth):
             return valid_Base64.decode('utf-8')
         except Exception:
             return None
+
+    def extract_user_credentials(
+            self, decoded_base64_authorization_header: str) -> (str, str):
+        """returns the user email and password from
+        the Base64 decoded value"""
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if not type(decoded_base64_authorization_header) == str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        user = decoded_base64_authorization_header.split(":")
+        return user[0], user[1]
