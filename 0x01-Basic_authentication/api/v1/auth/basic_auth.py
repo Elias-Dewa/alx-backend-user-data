477ce1ea36946b61883508df37b98ccd6fe5cda9
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-30 09:41:19 -0700

    create basic_auth file and update app file

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""a class to manage the API authentication"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """Defines a BasicAuth class that inherits from Auth"""
+    pass
