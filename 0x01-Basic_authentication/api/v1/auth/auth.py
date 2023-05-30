00cacf6e0ed44d3cdf515e5f32b8bfc295ae59fb
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-30 07:51:22 -0700

    Authentication: task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""a class to manage the API authentication"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """Defines the authentication mechanism"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths"""
+        if excluded_paths is None or not len(excluded_paths) or not path:
+            return True
+        for element in excluded_paths:
+            if element.endswith("*"):
+                if path.startswith(element[0:1]):
+                    return False
+
+    def authorization_header(self, request=None) -> str:
+        """returns None - request will be the Flask request object"""
+        if request:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request will be the Flask request object"""
+        return None
