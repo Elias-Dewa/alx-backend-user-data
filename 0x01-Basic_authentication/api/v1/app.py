bb5e0a2b0c2e9f046a0e1f08028855e3e539344e
Elias-Dewa <elias.dewa19@gmail.com>
2023-05-30 07:15:02 -0700

    Bugs are fixed for task 0, 1 and 2

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
-"""Basic Authentication on a simple API"""
-
-from flask import Flask, jsonify
+"""
+Route module for the API
+"""
+from os import getenv
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
 @app.errorhandler(401)
@@ -17,3 +24,16 @@ def unauthorized(error) -> str:
 def forbidden(error) -> str:
     """a method that handles forbidden errors"""
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,6 +5,18 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
+@app_views.route('/unauthorized', methods=['GET'])
+def unauthorized() -> str:
+    """the error handler for 401"""
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'])
+def forbidden() -> str:
+    """the error handler for 403"""
+    abort(403)
+
+
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
