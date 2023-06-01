7b61f024aa3f7ca9eed80b7a1906a92342f4f01b
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-01 06:12:12 -0700

    Session authentication: task 0

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""a class to manage the API authentication"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth():
+    """Defines the authentication mechanism"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns False - path and excluded_paths"""
+        if excluded_paths is None or not len(excluded_paths) or not path:
+            return True
+        for element in excluded_paths:
+            if "*" in element:
+                if path.startswith(element[0:1]):
+                    return False
+        if path in excluded_paths or f'{path}/' in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """return the value of the header request Authorization"""
+        if request:
+            return request.headers.get('Authorization')
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """returns None - request will be the Flask request object"""
+        return None
