20299576b43a443516dcee5d4e740f43bd2041ac
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-01 11:15:15 -0700

    Update session_auth.py - again and again

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -12,18 +12,18 @@ def login():
     """method to handles login routes for the Session authentication"""
     usr_email = request.form.get('email')
     usr_password = request.form.get('password')
-    if usr_email is None:
+    if not usr_email:
         return jsonify({"error": "email missing"}), 400
-    if usr_password is None:
+    if not usr_password:
         return jsonify({"error": "password missing"}), 400
 
     users = User.search(usr_email)
-    if users is None:
+    if not users:
         return jsonify({"error": "no user found for this email"}), 404
 
     for user in users:
         if not user.is_valid_password(usr_password):
-            return jsonify({"error": "wrong password"}), 401
+            return jsonify({"error": "wrong password"}),
         from api.v1.app import auth
         session_id = auth.create_session(user.id)
         session_name = getenv('SESSION_NAME')
