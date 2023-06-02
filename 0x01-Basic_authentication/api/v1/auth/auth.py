49bdc5026e0abb58b9ddefb8602967a1160403a6
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-02 13:40:26 -0700

    Update auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,7 +13,7 @@ class Auth():
         if excluded_paths is None or not len(excluded_paths) or not path:
             return True
         for element in excluded_paths:
-            if "*" in element:
+            if element.endswith("*"):
                 if path.startswith(element[0:1]):
                     return False
         if path in excluded_paths or f'{path}/' in excluded_paths:
