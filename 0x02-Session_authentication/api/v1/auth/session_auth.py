bde4a95b45ac332d33e0b7bead6f2c10a0be54f0
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-02 15:25:58 -0700

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -35,8 +35,11 @@ class SessionAuth(Auth):
         """method that deletes the user session / logout"""
         if request is None:
             return False
-        sessionID = self.session_cookie(request)
-        if not sessionID or not self.user_id_for_session_id(sessionID):
+        session_id = self.session_cookie(request)
+        if not session_id:
             return False
-        self.user_id_by_session_id.pop(sessionID)
+        usr_id = self.user_id_for_session_id(session_id)
+        if not usr_id:
+            return False
+        del self.user_id_by_session_id[session_id]
         return True
