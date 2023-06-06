f85058097ead511d24da48954fbf1b9fa8e55029
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-06 04:20:22 -0700

    task 5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -33,9 +33,11 @@ def before_request() -> str:
     if not auth.require_auth(path=request.path, excluded_paths=[
             '/api/v1/status/',
             '/api/v1/unauthorized/',
-            '/api/v1/forbidden/']):
+            '/api/v1/forbidden/',
+            '/api/v1/auth_session/login/']):
         return
-    if not auth.authorization_header(request):
+    if (not auth.authorization_header(request) and
+            not auth.session_cookie(request)):
         abort(401)
     if not auth.current_user(request):
         abort(403)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 """Define a class SessionAuth that inherits from Auth"""
+from uuid import uuid4
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """Represents a session authentication"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """a method to creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """a method that returns a User ID based on a Session ID"""
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
