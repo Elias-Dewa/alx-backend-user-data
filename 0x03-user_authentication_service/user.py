420d3375c8333eedda854fad3dc7df8970d8960e
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-07 02:52:32 -0700

    task 1

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-"""create a SQLAlchemy model named User for a database table named users"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
+"""
+Main file
+"""
 
-Base = declarative_base()
+from db import DB
+from user import User
 
+my_db = DB()
 
-class User(Base):
-    """Create a User instance for a database table named users"""
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    email = Column(String(250), nullable=False)
-    hashed_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+print(user_1.id)
+
+user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+print(user_2.id)
