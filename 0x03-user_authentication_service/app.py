72d939d274293a58555c73c07f0f1ad6f3085ccb
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-08 07:08:53 -0700

    Update app.py - task 7

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -6,7 +6,24 @@ from auth import Auth
 
 app = Flask(__name__)
 
+AUTH = Auth()
+
 
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": email, "message": "password already registered"})
