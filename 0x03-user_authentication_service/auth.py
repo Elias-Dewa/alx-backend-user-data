0bcf335fd67a70e091139186aa3387ad3c1f7ab6
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-08 07:21:06 -0700

    Update auth.py - task 10

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -39,6 +39,17 @@ class Auth:
             return True
         return False
 
+    def create_session(self, email: str) -> str:
+        """It takes an email string argument and returns
+        the session ID as a string"""
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=uuid)
+            return uuid
+        except NoResultFound:
+            return None
+
 
 def _generate_uuid() -> str:
     """generates a random uuid"""
