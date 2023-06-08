bde49fdcda8b3b5d96e9f5e9dc868992bc32e36a
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-08 07:58:49 -0700

    Update app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -77,6 +77,6 @@ def update_password():
 
     try:
         AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
     except Exception:
         abort(403)
-    return jsonify({"email": email, "message": "Password updated"}), 200
