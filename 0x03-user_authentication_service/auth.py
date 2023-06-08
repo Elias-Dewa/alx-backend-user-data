aa140bd5325e1599b18a03cd402b16c19dc9a81b
Elias-Dewa <elias.dewa19@gmail.com>
2023-06-08 07:17:04 -0700

    Update auth.py - task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -27,3 +27,13 @@ class Auth:
             user = self._db.add_user(email, _hash_password(password))
             return user
         raise ValueError(f"User {email} already exists")
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """returns boolean indicating whether the user is valid"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        if checkpw(password.encode(), user.hashed_password):
+            return True
+        return False
